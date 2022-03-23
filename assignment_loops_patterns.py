774744bf8f3fe19d8469cf7ad7b689458284f42d
abhayaryan4u <65472705+abhayaryan4u@users.noreply.github.com>
2022-03-23 16:30:25 +0530

    Update assignment_loops_patterns.py

--- assignment_loops_patterns.py
+++ assignment_loops_patterns.py
@@ -25,7 +25,7 @@ for i in range(0,5):
   for j in range(i,5):
     print(" ",end="")
   for j in range(i+1):
-    print("*",end="")
+    print("*",end=" ")
   for j in range(i):
-    print("*",end="")
+    print("",end="")
   print()
